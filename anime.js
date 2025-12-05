1cfa2e83d3b0e3821788b87d4fa7420ca2993757
Rudhrashini <rudhrashini@example.com>
2025-12-05 10:28:03 +0530

    4.0

---
+++ anime.js
@@ -0,0 +1,25 @@
+// count = 0
+// const cli = () => {
+//     count++;
+//     const heading = document.getElementById("data")
+//     heading.innerText = count;
+// }
+// const arr = () => {
+//     const heading = document.createElement("h1")
+//     heading.innerText = "Bitchhh"
+//     const cont = document.getElementById("container")
+//     cont.appendChild(heading);
+// }
+// document.addEventListener("click", () => {
+//     console.log("Hi");
+//     const heading = document.createElement("h1")
+//     heading.innerText = "Rudhraaaa"
+//     const cont = document.getElementById("container")
+//     cont.appendChild(heading);   
+// }
+// )
+
+const col=()=>{
+    const divi=document.getElementById("cont");
+    divi.style.backgroundColor="black"
+}
